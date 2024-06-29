d1affd9cd152b23512150cd5c3f9c611ce053fda
arpitnakrani <arpitnakrani1022@gmail.com>
2024-06-29 10:16:05 +0530

    latest 29/06/24

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ const con = mysql.createConnection({
 
 con.connect(function (err, res) {
 
-
+    
     if (err) throw err;
+    
 })
 
 
@@ -27,42 +28,80 @@ app.post('/adduser', (req, res) => {
     user = req.body.name;
     const query = "insert into touser (name) values ('" + user + "')";
 
-    con.query(query, (err, res) => {
+    con.query(query, (err, success) => {
         if (err) throw err;
 
-        console.log(res);
+        res.status(200).json({
+            massage:'User has added'
+        })
     });
 });
 
-app.get('/addtask', (req, res) => {
+app.post('/addtask', (req, res) => {
 
     userid = req.body.userid;
     task = req.body.taskName;
 
-    const Check = 'SELECT * FROM tasklist where taskname =? and userid='+userid;
+    const Check = 'SELECT * FROM tasklist where taskname =? and userid=' + userid;
     con.query(Check, [task], (err, ok) => {
         if (err) {
             res.status(500).json({ success: false, message: 'Failed to add task', error: err });
             return;
         }
         if (ok.length > 0) {
-           // console.log(ok)
+            // console.log(ok)
             res.status(200).json({ success: true, message: 'Task is Already exits' });
-            return;
+           // return;
+        }
+        else
+        {
+            const query = "insert into  tasklist (userid,taskname) values ('" + userid + "','" + task + "')";
+
+            con.query(query, (err, ok) => {
+                if (err) {
+                    res.status(500).json({ success: false, message: 'Failed to add task', error: err });
+                    return;
+                }
+                console.log(ok);
+                res.status(200).json({ success: true, message: 'Task added successfully' });
+            });
         }
     });
 
-    const query = "insert into  tasklist (userid,taskname) values ('"+userid+"','"+task+"')";
-
-    con.query(query,(err,ok)=>{
-        if (err) {
-            res.status(500).json({ success: false, message: 'Failed to add task', error: err });
-            return;
-        }
-        console.log(ok);
-        res.status(200).json({ success: true, message: 'Task added successfully' });
-    });
+    
 });
 
+app.post('/deleteTask', function (req, res) {
+    const id = req.body.id;
+    const query = 'delete from tasklist where t_id=' + id;
+    con.query(query, (err, success) => {
+        if (err) {
+            res.status(500).json({
+                massge: err
+            });
+        }
+
+        res.status(200).json({
+            status:true,
+            massage:'Delete Succesfully'    
+        })
+
+    })
+})
+
+app.post('/showtask',function(req,res){
+
+    const id = req.body.id;
+    const query = 'select taskname from tasklist where userid='+id;
+
+    con.query(query,function(err,success){
+        if(err) throw err;
+
+        res.status(200).json({
+            massage :'Data fetch Successfully',
+            data:success
+        })
+    })
+});
 
 app.listen(5000);
